4e8e16053c3cb4ae60fcdcdb733cadfa2252caaa
vir$i <egrvrb@gmail.com>
2025-10-31 15:05:47 +0300

    [answers] create answer logic

--- answers/views.py
+++ answers/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Answer, AnswerVote
+from .models import Answer
+from .models import AnswerVote
 from .forms import AnswerForm
 from questions.models import Question
 
