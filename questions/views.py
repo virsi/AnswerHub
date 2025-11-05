bee56cd2c1b0eacd09dab1fc04a0246b75b0996d
vir$i <egrvrb@gmail.com>
2025-11-05 16:27:04 +0300

    [questions][templates/questions] fixed paginations

--- questions/views.py
+++ questions/views.py
@@ -9,14 +9,14 @@ from answers.models import Answer
 
 def paginate(objects_list, request, per_page=10):
     paginator = Paginator(objects_list, per_page)
-    page_number = request.GET.get('page', 1)
+    page_number = request.GET.get('page')
 
     try:
-        page = paginator.page(page_number)
+        page = paginator.get_page(page_number)
     except PageNotAnInteger:
-        page = paginator.page(1)
+        page = paginator.get_page(1)
     except EmptyPage:
-        page = paginator.page(paginator.num_pages)
+        page = paginator.get_page(paginator.num_pages)
 
     return page
 
