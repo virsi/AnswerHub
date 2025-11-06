f734613a30c7278f39a7cd8a0aef84e25951215a
vir$i <egrvrb@gmail.com>
2025-11-06 09:42:53 +0300

    some style and routing fix

--- AnswerHub/urls.py
+++ AnswerHub/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('questions/', permanent=False)),
     path('admin/', admin.site.urls),
-    path('question', include('questions.urls')),
+    path('questions/', include('questions.urls')),
     path('users/', include('users.urls')),
     path('answers/', include('answers.urls')),
     path('tags/', include('tags.urls')),
-    path('', include('questions.urls')), #редирект
     path('search/', include('search.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- questions/urls.py
+++ questions/urls.py
@@ -5,9 +5,9 @@ app_name = 'questions'
 
 urlpatterns = [
     path('', views.question_list, name='list'),
-    path('/hot/', views.hot_questions, name='hot'),
-    path('/ask/', views.ask_question, name='ask'),
-    path('/<int:question_id>/', views.question_detail, name='detail'),
-    path('/<int:question_id>/vote/', views.vote_question, name='vote'),
-    path('/<int:question_id>/delete/', views.delete_question, name='delete'),
+    path('hot/', views.hot_questions, name='hot'),
+    path('ask/', views.ask_question, name='ask'),
+    path('<int:question_id>/', views.question_detail, name='detail'),
+    path('<int:question_id>/vote/', views.vote_question, name='vote'),
+    path('<int:question_id>/delete/', views.delete_question, name='delete'),
 ]
